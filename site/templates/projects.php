7a13ce04cc61f8d53d57c45bbd6cf173b2ce1a0c
Maximilian Koch <maximiliankochhome@gmail.com>
2024-10-30 13:53:34 +0100

    basic settup of the adimn pannel with new layouts for project(s) and project pages.

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -7,16 +7,18 @@ $height = 500;
 
 <main class="main-wrapper">
     <ul class="projects">
+
         <?php foreach ($page->children()->listed() as $project): ?>
             <li>
                 <a href="<?= $project->url() ?>">
                     <figure>
-                            <?= $project->image()->crop($width, $height) ?>
+                        <?= $project->image()->crop($width, $height) ?>
                         <figcaption><?= $project->title() ?></figcaption>
                     </figure>
                 </a>
             </li>
         <?php endforeach ?>
+
     </ul>
 </main>
 
