60a019fbc46c9e597c1bd065213556eec0713dce
Maximilian Koch <maximiliankochhome@gmail.com>
2025-02-18 16:54:47 +0100

    start project list front end

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -5,24 +5,24 @@ $height = 500;
 
 <div class="projects">
     <?php foreach ($projects as $project): ?>
-    
-            <a class="projects-link" href="<?= $project->url() ?>">
-                <figure class="projects-figure">
-                    <?= $project->images()->template('gallery-image')->first()->thumb([
-                        'autoOrient' => true,
-                        'width' => $width, 
-                        'height' => $height,
-                        'crop' => true,
-                        'quality' => 80, /*For faster loading*/ 
-                        'fromat' => 'webp', /*For faster loading*/  
-                    ])
-                        ?>
-                    <figcaption class="projects-caption">
-                        <?= $project->title() ?><br>
-                        <small><?= $project->type() ?></small>
-                    </figcaption>
-                </figure>
-            </a>
-        
+
+        <a class="projects-link" href="<?= $project->url() ?>">
+            <figure class="projects-figure">
+                <?= $project->images()->template('gallery-image')->first()->thumb([
+                    'autoOrient' => true,
+                    'width' => $width,
+                    'height' => $height,
+                    'crop' => true,
+                    'quality' => 80, /*For faster loading*/
+                    'fromat' => 'webp', /*For faster loading*/
+                ])
+                ?>
+                <figcaption class="projects-caption">
+                    <?= $project->title() ?><br>
+                    <small class="projects-subtext"><?= $project->type() ?></small>
+                </figcaption>
+            </figure>
+        </a>
+
     <?php endforeach ?>
-</>
+</div>
