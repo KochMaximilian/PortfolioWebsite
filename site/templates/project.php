d79665cbe89cb553c8ebbf04eecdb4b5c43568fd
Maximilian Koch <maximiliankochhome@gmail.com>
2025-02-21 20:40:49 +0100

    base layout for projct detail main section

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,102 +1,49 @@
 <?php snippet('header') ?>
 <div class="wrapper">
-
-
-
-    <?php
-    $imageWidth = 600;
-    $imageHeight = 600;
-    ?>
     <div class="project-layout">
         <main>
             <section class="content-block">
-                <h1><?= $page->title() ?></h1>
+                <h1><?= $page->name() ?></h1>
             </section>
 
-            <section class="project-visuals-block">
+            <section class="project-main-container">
                 <div class="project-gallery">
                     <?php if ($embed = $page->embedlink()->toEmbed()): ?>
-                            <?= $embed->code() ?>
+                        <?= $embed->code() ?>
                     <?php else: ?>
                         <div>
                             <?php $coverImage = $page->images()->template('gallery-image')->limit(1); ?>
                             <?php foreach ($coverImage as $image): ?>
-                                    <a href="<?= $image->url() ?>">
-                                        <img
-                                            loading="lazy"
-                                            alt="<?= $image->alt() ?>"
-                                            class="project-image"
-                                            src="<?= $image->resize($imageWidth, $imageHeight)->url() ?>">
-                                    </a>
-                            <?php endforeach ?>
-                        </div>
-                    <?php endif ?>
-                </div>
-            </section>
-
-            <section>
-                <div class="project-info">
-                    <?= $page->context() ?>
-                    <dl>
-                        <dt>Title:</dt>
-                        <dd><?= $page->name()->upper() ?></dd>
-
-                        <dt>Description:</dt>
-                        <dd><?= $page->description() ?></dd>
-
-                        <dt>Project Type</dt>
-                        <dd><?= $page->type() ?></dd>
-
-                        <dt>Genre:</dt>
-                        <?php foreach ($page->genre()->split(',') as $genre): ?>
-                            <dd><?= $genre ?></dd>
-                        <?php endforeach; ?>
-
-                        <dt>Platform:</dt>
-                        <?php foreach ($page->platform()->split(',') as $platfrom): ?>
-                            <dd><?= $platfrom ?></dd>
-                        <?php endforeach; ?>
-
-                        <dt>Year:</dt>
-                        <dd><?= $page->year() ?></dd>
-
-                        <dt>Project Length:</dt>
-                        <dd><?= $page->projectLength() ?></dd>
-
-                        <dt>Area of Focus:</dt>
-                        <?php foreach ($page->focus()->split(',') as $focus): ?>
-                            <dd><?= $focus ?></dd>
-                        <?php endforeach; ?>
-
-                        <dt>Game Engine:</dt>
-                        <dd><?= $page->engine() ?></dd>
-
-                        <?php if ($page->links()->isNotEmpty()): ?>
-                            <dt>Links:</dt>
-                            <dd><a href="<?= $page->links() ?>"><?= $page->links() ?></a></dd>
-                        <?php endif ?>
-                    </dl>
-                </div>
-            </section>
-
-            <section>
-                <ul>
-                    <?php $coverImage = $page->images()->template('showcase-image'); ?>
-                    <?php foreach ($coverImage as $image): ?>
-                        <li>
-                            <a href="<?= $image->url() ?>">
                                 <img
                                     loading="lazy"
                                     alt="<?= $image->alt() ?>"
                                     class="project-image"
-                                    src="<?= $image->resize($imageWidth, $imageHeight)->url() ?>">
-                            </a>
-                        </li>
-                    <?php endforeach ?>
-                </ul>
+                                    src="<?= $image->resize(600, 600)->url() ?>">
+
+                            <?php endforeach ?>
+                        </div>
+                    <?php endif ?>
+                </div>
+
+                <div class="project-info">
+    <dl>
+        <div><dt>Title:</dt> <dd><?= $page->name() ?></dd></div>
+        <div><dt>Description:</dt> <dd><?= $page->type() ?></dd></div>
+        <div><dt>Genre:</dt> <dd><?php foreach ($page->genre()->split(',') as $genre): ?> <?= $genre ?> <?php endforeach; ?></dd></div>
+        <div><dt>Platforms:</dt> <dd><?php foreach ($page->platform()->split(',') as $platform): ?> <?= $platform ?> <?php endforeach; ?></dd></div>
+        <div><dt>Release Year:</dt> <dd><?= $page->year() ?></dd></div>
+        <div><dt>Project Duration:</dt> <dd><?= $page->duration() ?></dd></div>
+        <div><dt>Area of Focus:</dt> <dd><?php foreach ($page->focus()->split(',') as $focus): ?> <?= $focus ?> <?php endforeach; ?></dd></div>
+        <div><dt>Game Engine:</dt> <dd><?= $page->engine() ?></dd></div>
+        <?php if ($page->links()->isNotEmpty()): ?>
+            <div><dt>Additional Links:</dt> <dd><a href="<?= $page->links() ?>"><?= $page->links() ?></a></dd></div>
+        <?php endif; ?>
+    </dl>
+</div>
+
             </section>
 
-
+   
         </main>
     </div>
 </div>
