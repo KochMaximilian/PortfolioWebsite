e5a9a4fad6ebcc09e175d3bc51e1362cd75c5e0a
Maximilian Koch <maximiliankochhome@gmail.com>
2025-03-14 17:58:37 +0100

    update projects overview layout to be responsive (figures)

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,21 +1,55 @@
 <div class="projects">
     <?php foreach ($projects as $project): ?>
-
         <a class="projects-link" href="<?= $project->url() ?>">
             <figure class="projects-figure">
-                <?= $project->images()->template('gallery-image')->first()->thumb([
-                    'autoOrient' => true,
-                    'width' => 500,
-                    'height' => 500,
-                    'crop' => true,
-                    'quality' => 50, /*For faster loading*/
-                    'driver' => 'im',
-                    'fromat' => 'webp', /*For faster loading*/
-                ])
-                ?>
+                <img 
+                    src="<?= $project->images()->template('gallery-image')->first()->thumb([
+                        'autoOrient' => true,
+                        'width' => 500,
+                        'height' => 500,
+                        'crop' => true,
+                        'quality' => 60,
+                        'format' => 'webp',
+                        'driver' => 'im'
+                    ])->url() ?>"
+                    alt="<?= $project->name() ?>"
+                    srcset="
+                        <?= $project->images()->template('gallery-image')->first()->thumb([
+                            'width' => 300,
+                            'height' => 300,
+                            'crop' => true,
+                            'quality' => 50,
+                            'format' => 'webp',
+                        ])->url() ?> 300w,
+                        <?= $project->images()->template('gallery-image')->first()->thumb([
+                            'width' => 500,
+                            'height' => 500,
+                            'crop' => true,
+                            'quality' => 60,
+                            'format' => 'webp',
+                        ])->url() ?> 500w,
+                        <?= $project->images()->template('gallery-image')->first()->thumb([
+                            'width' => 700,
+                            'height' => 700,
+                            'crop' => true, 
+                            'quality' => 70,
+                            'format' => 'webp',
+                        ])->url() ?> 700w,
+                        <?= $project->images()->template('gallery-image')->first()->thumb([
+                            'width' => 900,
+                            'height' => 900,
+                            'crop' => true,
+                            'quality' => 75,
+                            'format' => 'webp',
+                        ])->url() ?> 900w
+                    "
+                    sizes="(max-width: 576px) 100vw, 
+                           (max-width: 992px) 50vw, 
+                           33vw"
+                >
                 <figcaption class="projects-caption">
                     <h5><?= $project->name() ?></h5>
-                    <h6 class="projects-subtext"><?= $project->type() ?> &nbsp;|&nbsp; <?= $project->year() ?> </>
+                    <h6 class="projects-subtext"><?= $project->type() ?> &nbsp;|&nbsp; <?= $project->year() ?></h6>
                 </figcaption>
                 <div class="project-details">
                     <div class="tag-container">
