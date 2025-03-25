8a54b95cb162e51da6ffa518101b16ed74afc51d
Maximilian Koch <maximiliankochhome@gmail.com>
2025-03-25 16:36:33 +0100

    fixed issue with different font size on pages

--- site/snippets/footer-content.php
+++ site/snippets/footer-content.php
@@ -1,14 +1,6 @@
 <footer>
     <!--Mobile Nav Start -->
-    <div>
-        <ul class="navbar-nav">
-            <?php foreach ($site->children()->listed() as $item): ?>
-                <li class="navbar-item">
-                    <a class="nav-link <?= $item->url() == $page->url() ? 'active' : '' ?>" href="<?= $item->url() ?>"><?= $item->title() ?></a>
-                </li>
-            <?php endforeach ?>
-        </ul>
-    </div>
+    <?php snippet('navbar-mobile') ?>
     <!--Mobile Nav End -->
     <div class="footer-container">
         <div class="footer-left">
--- site/snippets/navbar-mobile.php
+++ site/snippets/navbar-mobile.php
@@ -1,18 +1,9 @@
-
-    <div class="nav-container">
-        <nav class="navbar">
-            <div class="logo-container">
-                <a class="logo" href="<?= $site->url() ?>"><?= $site->author()?> <span>&nbsp; | &nbsp;</span> <?= $site->banner() ?></a>
-            </div>
-            
-            <ul class="navbar-nav">
-                <?php foreach ($site->children()->listed() as $item): ?>
-                    <li class="navbar-item">
-                        <a class="nav-link <?= $item->url() == $page->url() ? 'active' : '' ?>"  href="<?= $item->url() ?>"><?= $item->title() ?></a>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-
-    
-        </nav>
-    </div>
+<div class="navbar-mobile">
+    <ul class="navbar-nav">
+        <?php foreach ($site->children()->listed() as $item): ?>
+            <li class="navbar-item">
+                <a class="nav-link <?= $item->url() == $page->url() ? 'active' : '' ?>" href="<?= $item->url() ?>"><?= $item->title() ?></a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</div>
