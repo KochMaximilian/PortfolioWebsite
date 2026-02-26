fae767985f36d2d20f4e58c548394febd7e8857d
Maximilian Koch <maximiliankochhome@gmail.com>
2026-02-26 23:43:32 +0100

    implemented table block + improvemtns to the headings + text imrpovments + option to choose size for img or video block + bug fixes and css updates

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -12,8 +12,11 @@ $src   = $isGif ? $image->url() : $image->thumb([
 $slideDesc    = $block->description()->isNotEmpty() ? (string)$block->description()->kirbytext() : null;
 $descPosition = $block->desc_position()->isNotEmpty() ? $block->desc_position()->value() : 'bottom';
 $altText      = $block->alt()->isNotEmpty() ? $block->alt()->value() : $image->alt()->value();
+$size         = $block->size()->isNotEmpty() ? $block->size()->value() : 'full';
+$figid        = $block->figid()->isNotEmpty() ? trim($block->figid()->value()) : null;
+$anchorId     = $figid ? 'fig-' . htmlspecialchars($figid) : null;
 ?>
-<figure class="devlog-figure">
+<figure class="devlog-figure devlog-figure--<?= $size ?>"<?= $anchorId ? ' id="' . $anchorId . '"' : '' ?>>
   <a href="<?= $image->url() ?>"
     data-gallery="devlog-gallery"
     <?= $slideDesc ? 'data-description="' . str_replace('"', '&quot;', $slideDesc) . '"' : '' ?>
@@ -25,7 +28,14 @@ $altText      = $block->alt()->isNotEmpty() ? $block->alt()->value() : $image->a
       loading="lazy"
       class="devlog-image">
   </a>
-  <?php if ($block->caption()->isNotEmpty()): ?>
-    <figcaption class="devlog-figcaption"><?= $block->caption() ?></figcaption>
+  <?php if ($block->caption()->isNotEmpty() || $figid): ?>
+    <figcaption class="devlog-figcaption">
+      <?php if ($figid): ?>
+        <span class="devlog-fig-id">Fig.&nbsp;<?= htmlspecialchars($figid) ?></span>
+      <?php endif ?>
+      <?php if ($block->caption()->isNotEmpty()): ?>
+        <?= $block->caption() ?>
+      <?php endif ?>
+    </figcaption>
   <?php endif ?>
 </figure>
