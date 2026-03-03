276d7ee562fd68a536a6f67966125bb65aca0239
Maximilian Koch <maximiliankochhome@gmail.com>
2026-03-04 00:22:15 +0100

    added anchors to headlines and some css adjustments

--- site/snippets/blocks/heading.php
+++ site/snippets/blocks/heading.php
@@ -1,2 +1,13 @@
-<?php $level = $block->level()->or('h2') ?>
-<<?= $level ?> class="devlog-heading devlog-heading--<?= $level ?>"><?= $block->text() ?></<?= $level ?>>
+<?php
+$level    = $block->level()->or('h2');
+$rawText  = strip_tags((string)$block->text());
+$customId = $block->anchorid()->isNotEmpty() ? trim($block->anchorid()->value()) : null;
+$anchorId = $customId ? Str::slug($customId) : Str::slug($rawText);
+$hasAnchorBadge = $customId !== null;
+?>
+<<?= $level ?> class="devlog-heading devlog-heading--<?= $level ?><?= $hasAnchorBadge ? ' has-anchor-badge' : '' ?>"<?= $anchorId ? ' id="' . htmlspecialchars($anchorId) . '"' : '' ?>>
+  <?php if ($hasAnchorBadge): ?>
+    <a href="#<?= htmlspecialchars($anchorId) ?>" class="devlog-heading-anchor"><?= htmlspecialchars($customId) ?></a>
+  <?php endif ?>
+  <span class="devlog-heading-text"><?= $block->text() ?></span>
+</<?= $level ?>>
