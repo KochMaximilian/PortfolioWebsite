4fb2b3f50fe95dfaf4f176f185ee9619a222ce6a
Maximilian Koch <maximiliankochhome@gmail.com>
2026-03-07 16:56:34 +0100

    changes to animation + fix sociial icon on mobile + logo position on mobile + next prejct button mobile changes

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -1,7 +1,22 @@
 <div class="nav-container">
     <nav class="navbar">
         <div class="logo-container">
-            <a class="logo" href="<?= $site->url() ?>"><?= $site->author()?> <span>&nbsp; | &nbsp;</span> <?= $site->banner() ?></a>
+            <?php
+            // Match navbar logo to the site variant (same as footer)
+            $logoMap = [
+                'piece' => 'LogoPiece.svg',
+                'shaka' => 'LogoShaka.svg',
+                'ok'    => 'LogoOK.svg',
+            ];
+            $variant = $GLOBALS['siteVariant'] ?? 'piece';
+            $navbarLogo = $logoMap[$variant];
+            ?>
+            <a class="logo" href="<?= $site->url() ?>">
+                <span class="logo-name"><?= $site->author() ?></span>
+                <span class="logo-divider">&nbsp; | &nbsp;</span>
+                <img class="navbar-logo-mobile" src="<?= $site->url() ?>/assets/img/<?= $navbarLogo ?>" alt="Logo">
+                <span class="logo-banner"><?= $site->banner() ?></span>
+            </a>
         </div>
         
         <?php snippet('navbar-desktop') ?>
